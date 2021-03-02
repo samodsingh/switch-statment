b8da0d6e44ce6eb4d60f4ceffd5348752147460d
samod singh <samod0098@gmail.com>
2021-03-03 03:54:12 +0530

    switch

---
+++ my.js
@@ -0,0 +1,14 @@
+    var day = "sun";
+
+    switch (day){
+        case "Mon":
+            document.write("working day")
+            break;
+                case "sat" :
+                case "sun":
+                document.write("Holi day")
+                break;
+                default:
+                    document.write("wrong")
+    }
+        
